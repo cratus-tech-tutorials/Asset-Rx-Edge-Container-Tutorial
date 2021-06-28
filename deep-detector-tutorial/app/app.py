95ef3da9d5aeb047649fc0c9dc86a202449f5a2d
gibsoncratus <gibson@cratustech.com>
2021-06-28 14:49:58 -0700

    new robustification test

--- deep-detector-tutorial/app/app.py
+++ deep-detector-tutorial/app/app.py
@@ -24,44 +24,47 @@ NET.eval()
 
 local_docker_network_ip = None
 def get_target():
-    global local_docker_network_ip
-    if (not local_docker_network_ip):
-        local_docker_network_ip = socket.gethostbyname(socket.gethostname())    
-    subnet_mask = local_docker_network_ip[:-2]
-    target = subnet_mask + ".0" + str(3)
-    return target
+	global local_docker_network_ip
+	if (not local_docker_network_ip):
+		local_docker_network_ip = socket.gethostbyname(socket.gethostname())    
+	subnet_mask = local_docker_network_ip[:-2]
+	target = subnet_mask + ".0" + str(3)
+	return target
 
 @app.route("/get")
 def get():
-    try:
-        upstream_container = f"http://{get_target()}:8080/get"
-        response = requests.get(upstream_container)
-        if response.status_code == 200:
-            j = response.json()
-            if "image" in j:
-                im_64 = j["image"]
-                im_raw = base64.b64decode(im_64)
-                im = Image.open(io.BytesIO(im_raw))
-                im = im.resize(IM_SCALE)
-                im = ImageOps.grayscale(im)
-                im = np.array(im) / 255.0
-                im = torch.tensor(im, dtype=torch.float32).unsqueeze(0).unsqueeze(0)#.cuda().half()
+	try:
+		upstream_container = f"http://{get_target()}:8080/get"
+		response = requests.get(upstream_container)
+		if response.status_code == 200:
+			j = response.json()
+			try:
+				if "image" in j:
+					im_64 = j["image"]
+					im_raw = base64.b64decode(im_64)
+					im = Image.open(io.BytesIO(im_raw))
+					im = im.resize(IM_SCALE)
+					im = ImageOps.grayscale(im)
+					im = np.array(im) / 255.0
+					im = torch.tensor(im, dtype=torch.float32).unsqueeze(0).unsqueeze(0)#.cuda().half()
 
-                with torch.no_grad():
-                    inf = NET(im).item()
-                if "inference" not in j:
-                    j["inference"] = []
-                print(inf)
-                cls = "class_1" if inf <= 0 else "class_2"
-                j["inference"].append({"class": cls})
+					with torch.no_grad():
+						inf = NET(im).item()
+					if "inference" not in j:
+						j["inference"] = []
+					print(inf)
+					cls = "class_1" if inf <= 0 else "class_2"
+					j["inference"].append({"class": cls})
+			except Exception as e:
+				print(e)
+			finally:
+				return app.response_class(
+					response=json.dumps(j),
+					status=200,
+					mimetype='application/json')
 
-            return app.response_class(
-                response=json.dumps(j),
-                status=200,
-                mimetype='application/json')
-
-    except Exception as e:
-        print(e)
+	except Exception as e:
+		print(e)
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=8080)
+	app.run(debug=False, host='0.0.0.0', port=8080)
